b07742bacc79060bddda493a90c91e0e0bc21963
Daniel Selph <daniel.p.selph@gmail.com>
2020-05-29 09:57:33 -0600

    Complete US2, add calorie count helper method with model test

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -20,6 +20,14 @@ RSpec.describe "dishes show page" do
       expect(page).to have_content(@ingredient2.name) 
       expect(page).to have_content(@chef1.name) 
     end
+
+    it "can see total calories" do
+      
+      visit "/dishes/#{@dish1.id}"
+
+      expect(page).to have_content("Total Calories: 150") 
+
+    end
   end
 end
 
@@ -29,3 +37,8 @@ end
 # When I visit a dish's show page
 # I see a list of ingredients for that dish
 # and the chef's name
+
+# Story 2 of 3
+# As a visitor
+# When I visit a dish's show page
+# I see the total calorie count for that dish.
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe Dish, type: :model do
   describe "relationships" do
     it {should belong_to :chef}
   end
+  describe "methods" do
+    it "#calorie_count" do
+    chef1 = Chef.create(name: "Gordon")
+    dish1 = chef1.dishes.create(name: "Beef Wellington", description: "Excellent")
+    ingredient1 = Ingredient.create(name: "Beef", calories: 100)
+    ingredient2 = Ingredient.create(name: "Dough", calories: 50)
+    dish_ingredients1 = DishIngredient.create(dish: dish1, ingredient: ingredient1)
+    dish_ingredients2 = DishIngredient.create(dish: dish1, ingredient: ingredient2)
+
+    expect(dish1.calorie_count).to eq(150) 
+    end
+    
+    
+  end
+  
 end
